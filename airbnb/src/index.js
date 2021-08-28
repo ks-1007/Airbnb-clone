6d07cfbce2d761ba9308c11821fdb740e2d60dec
samy721 <bhandarisumit049@gmail.com>
2021-08-28 19:03:08 +0530

    started functionality of payment page

--- airbnb/src/index.js
+++ airbnb/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { ConfirmAndPay } from './Pages/confirmAndPay';
 
 ReactDOM.render(
-  <React.StrictMode>
-    < App/>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <ConfirmAndPay/>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
