5f128976e2208b8f9499e37e052fc2b5d32eebab
vamsi <vamsinagendra2@gmail.com>
2021-08-29 11:39:18 +0530

    home

--- airbnb/src/Components/home/Top.jsx
+++ airbnb/src/Components/home/Top.jsx
@@ -1,6 +1,8 @@
 
 import styles from "./home.module.css"
-// import { Navbar } from "./Nav"
+import { Navbar } from "./Nav"
+import { Search } from "./Search"
+// import { Navsearch } from "./SearchDiv"
 // import { Modal, Paper, Typography } from '@material-ui/core';
 
 export function Top() {
@@ -12,20 +14,15 @@ export function Top() {
                     <a href="a">Get the latest on our COVID-19 response</a>
                 </div>
             </div>
-            {/* <Modal open={true} aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        //className={classes.modal}
-        //onClose={handleClose}
-        closeAfterTransition
-        //BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}>
-                <Paper><Typography>heii</Typography></Paper>
-            </Modal> */}
             <div className={styles.imgdiv}>
                 <img src="https://a0.muscache.com/im/pictures/57b9f708-bb12-498c-bc33-769f8fc43e63.jpg?im_w=2560" alt="" />
-                {/* <Navbar/> */}
+                <Navbar />
+                <Search />
+                
+                <div className={styles.flexible}>
+                    <p>Not sure where to go? Perfect.</p>
+                    <button><span className={styles.gradient}>I’m flexible</span></button>
+                </div>
             </div>
         </div>
     )
