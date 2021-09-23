e2f30b2ff6804fe98df136ccb9182b9eaa8f023a
Kumar Saurav <krsaurav1007@gmail.com>
2021-09-23 10:07:47 +0530

    fixed cost calculations at checkout

--- airbnb/src/Components/confirmAndPayComp/daysCalculator.jsx
+++ airbnb/src/Components/confirmAndPayComp/daysCalculator.jsx
@@ -2,7 +2,7 @@ export const DaysCalculator = ({ checkIn, checkOut }) => {
   //   console.log(checkIn, checkOut, "hello")
 
   // To calculate the time difference of two dates
-  const Difference_In_Time = checkOut.getTime() - checkIn.getTime()
+  const Difference_In_Time = checkOut.getTime() - checkIn.getTime() + 1
 
   // To calculate the no. of days between two dates
   const Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24)
